2b277106dbfbe48957bfb80203a6d765b9eacf02
William Byrd <webyrd@gmail.com>
2024-11-17 02:51:06 -0500

    added template/copy-termo implementation of SKI reducer

--- tests.scm
+++ tests.scm
@@ -635,8 +635,51 @@
      S
      S)))
 
-;; TODO -- rewrite the SKI reducer so that `contracto` uses
-;; `copy-termo` and templates
+
+(test "cl-reducer-second-version-1"
+  (run* (rv)
+    (eval-programo
+     `(run* (q)
+        (letrec-rel ((contracto (T T^)
+                       (fresh (rule-template rule-template-copy x y z)
+                         (== (cons T (cons '=> (cons T^ '()))) rule-template-copy)
+                         (conde
+                           ((== (cons
+                                  (cons 'I (cons x '()))
+                                  (cons '=> (cons x '())))
+                               rule-template))
+                           ((== (cons
+                                  (cons (cons 'K (cons x '())) (cons y '()))
+                                  (cons '=> (cons x '())))
+                                rule-template))
+                           ((== (cons
+                                  (cons (cons (cons 'S (cons x '())) (cons y '())) (cons z '()))
+                                  (cons
+                                    '=>
+                                    (cons (cons (cons x (cons z '())) (cons (cons y (cons z '())) '())) '())))
+                                rule-template)))
+                         (copy-termo rule-template rule-template-copy))))
+          (letrec-rel ((->1wo (T T^)
+                         (conde
+                           ((contracto T T^))
+                           ((fresh (M N P)
+                              (== (cons M (cons N '())) T)
+                              (conde
+                                ((== (cons P (cons N '())) T^)
+                                 (->1wo M P))
+                                ((== (cons M (cons P '())) T^)
+                                 (->1wo N P))))))))
+            (letrec-rel ((->wo (M N)
+                           (conde
+                             ((== M N))
+                             ((fresh (P) (->1wo M P) (->wo P N))))))
+              (->wo '(I (I S)) q)))))
+     rv))
+  '(((I (I S))
+     (I S)
+     (I S)
+     S
+     S)))
 
 
 #|
